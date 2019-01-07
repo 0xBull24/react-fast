b215702b2fa1ab06a516ae48aabd94a361c4ffbf
D.Biles <darron.biles@gmail.com>
2019-01-07 00:18:48 -0500

    [Fin] - People cards

---
+++ src/components/PeopleCard/peopleCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import './PeopleCard.css'
+
+const PeopleCard = props => (
+    <div className='card'>
+        <div className='card-image'>
+            <img alt={props.name} src={props.image} />
+        </div>
+        <div className='content'>
+            <ul>
+                <li>
+                    <strong>Name: </strong> {props.name}
+                </li>
+                <li>
+                    <strong>Location: </strong> {props.name}
+                </li>
+            </ul>
+        </div>
+        <span onClick={() => {props.shufflePeople(props.shufflePeople)}} className='pulse'>
+            Shuffle
+        </span>
+    </div>
+);
+
+export default PeopleCard;
